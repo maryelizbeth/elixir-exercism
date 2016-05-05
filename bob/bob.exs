f3988e98fdea54abee8506cb8980d4f9121a2928
Mary Elizabeth Cutrali <mary.cutrali@gmail.com>
2016-05-04 21:21:58 -0400

    whoops. regex is punishment

--- bob/bob.exs
+++ bob/bob.exs
@@ -1,16 +1,17 @@
 defmodule Bob do
   def hey(input) do
     cond do
-        true -> match_regex(input)
-    end
-  end
+         String.upcase(input) == input && Regex.match?(~r/[\p{Lu}]/, input) ->
+          "Whoa, chill out!"
 
-  def match_regex(phrase) do
-    cond do
-        Regex.match?(~r/\?$/, phrase)  -> "Sure."
-        Regex.match?(~r/^\S[^a-z]*$/, phrase) -> "Whoa, chill out!"
-        Regex.match?(~r/^\s+$|^$/, phrase) -> "Fine. Be that way!"
-        Regex.match?(~r/[\d*\W^[^a-zA-Z]*$/, phrase) -> "Whatever."
+        Regex.match?(~r/[?]$/, input) ->
+          "Sure."
+
+        !Regex.match?(~r/\S/, input) ->
+          "Fine. Be that way!"
+
+        true ->
+          "Whatever."
     end
   end
 end
